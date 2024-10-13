87b8405d86266c448b776ea45b4631fd8407c4da
Hau Pham <phaucau2311@gmail.com>
2024-10-13 18:10:38 +0700

    dist/src/main

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Req() req) {
+  getHello(@Req() req:any) {
     return this.appService.getHello(req);
     // return {
     //   message: 'Hello World!',
